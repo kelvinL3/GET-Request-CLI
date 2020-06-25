9e9396a320622c2cf9d8da116d8baa3a604de2dd
KelvinL3 <kelvinliu1234@gmail.com>
2020-06-25 17:57:46 -0400

    working solution

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="msr",
+    version="1.0.0",
+    author="Kelvin",
+    author_email="author@example.com",
+    description="Symops challenge",
+    long_description="...",
+    long_description_content_type="python",
+    url="example",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
